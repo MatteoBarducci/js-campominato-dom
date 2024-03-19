17d516fde26f4b720f2c2c554ad43285b72f64e5
MatteoBarducci <mbardu1301@gmail.com>
2024-03-19 10:12:23 +0100

    feat: add comments

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Per preparare il gioco seleziono dall'HTML la griglia, il bottore 'gioca' e il livello di difficoltà scelto dall'utente nella select.
 const grid = document.querySelector('.grid')
 const btn = document.querySelector('#btn');
 let difficulty = document.querySelector('#difficulty');
+// Assegno una score pari a zero che verrà incrementata di 1 ogni qualvolta l'utente clicca su un quadrato 'non bomba'
 let score = 0;
 
-
+// Al click del bottone genero la griglia di quadrati in base alla difficoltà inserita dall'utente. 
+// La griglia, essendo un quadrato, deve avere il lato con lunghezza pari a √(numeri nella griglia) quadrati
 btn.addEventListener('click', function(){
     grid.innerHTML = ''
     let value = difficulty.value;
-
+    let numberPerRow = Math. sqrt(value)
+    // Creo un array di 16 bombe univoche e casuali, che hanno come valore minimo 1 e come valore massimo il numero maggiore della griglia
     let bombs = []
     while(bombs.length < 16){
         let bomb = Math.floor(Math.random() * (value - 1 + 1)) + 1;
@@ -20,16 +24,17 @@ btn.addEventListener('click', function(){
             bombs.push(bomb)
         }
     }
-
-    let numberPerRow = Math. sqrt(value)
-    
+    // Creo un ciclo for che, utilizzando una funzione, genera un quadrato che ha come lato 100% / √(numeri nella griglia) 
     for(let i = 1; i <= value; i++){
         const newSquare = generateSquare(i);
-        
         newSquare.style.width = `calc(100% / ${numberPerRow})`
         newSquare.style.height = `calc(100% / ${numberPerRow})`
-        
+        // Aggiungo i quadrati alla griglia
         grid.append(newSquare);
+        // Al click il quadrato si colora di blu se questo non è una bomba. 
+        // Ovvero, se il numero del quadrato cliccato non è compreso nell'array di bombe. 
+        // Altrimenti il quadrato si colora di rosso
+        // Inoltre, per ogni click su un quadrato non bomba, la score aumenta di uno.
         newSquare.addEventListener ('click', function(){
             if (!bombs.includes(i)){
                 score += 1
@@ -45,6 +50,8 @@ btn.addEventListener('click', function(){
     
 })
 
+// La funzione genera un elemento del DOM, un div con classe square.
+// All'interno di questo viene aggiunto uno span con il numero dato all'interno
 function generateSquare(number){
     const newSquare = document.createElement('div');
     newSquare.classList.add('square');
